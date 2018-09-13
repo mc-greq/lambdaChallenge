8b052f7e4416248904fac6636822a98c20d67210
Grzegorz <greq19@gmail.com>
2018-09-13 19:30:07 +0200

    Initial commit

---
+++ src/com/projekty/Main.java
@@ -0,0 +1,32 @@
+package com.projekty;
+
+import java.util.function.Function;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Runnable runnable = () -> {
+          String myString = "Lets split this up into an array";
+          String[] parts = myString.split(" ");
+          for(String part: parts){
+              System.out.println(part);
+          }
+        };
+
+        runnable.run();
+
+        Function<String, String> lambdaFunction = s -> {
+            StringBuilder returnVal = new StringBuilder();
+            for(int i = 0; i < s.length(); i++){
+                if(i % 2 == 1){
+                    returnVal.append(s.charAt(i));
+                }
+            }
+
+            return returnVal.toString();
+        };
+
+        System.out.println(lambdaFunction.apply("1234567890"));
+    }
+}
